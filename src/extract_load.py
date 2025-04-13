52e19497e822f19765bcc56592f6e1d6af831570
Enzo Muno <enzomacedomuno@gmail.com>
2025-04-13 18:02:47 -0300

    feat: Adicionar o cron job do dbt

--- src/extract_load.py
+++ src/extract_load.py
@@ -11,7 +11,7 @@ load_dotenv()
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
+DB_HOST = os.getenv('DB_HOST', '5432')
 DB_PORT = os.getenv('DB_PORT')
 DB_SSLMODE = os.getenv('DB_SSLMODE')
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSLMODE}"
